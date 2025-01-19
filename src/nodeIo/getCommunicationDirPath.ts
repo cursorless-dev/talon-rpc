da9a1ed5f6acad27cb79b6e500ee98d6fa53dd79
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-01-19 09:53:05 +0100

    Extended support for temp directory

--- src/nodeIo/getCommunicationDirPath.ts
+++ src/nodeIo/getCommunicationDirPath.ts
@@ -1,12 +1,16 @@
-import { tmpdir, userInfo } from "node:os";
-import { join } from "node:path";
+import * as os from "node:os";
+import * as path from "node:path";
 
-export function getCommunicationDirPath(name: string): string {
-    const info = userInfo();
+export function getCommunicationDirPath(name: string, tmpDir?: string): string {
+    const info = os.userInfo();
 
     // NB: On Windows, uid < 0, and the tmpdir is user-specific, so we don't
     // bother with a suffix
     const suffix = info.uid >= 0 ? `-${info.uid}` : "";
 
-    return join(tmpdir(), `${name}${suffix}`);
+    // Include TMPDIR to match the implementation of Pythons
+    // `tempfile.gettempdir()` that we use client side.
+    const tmpDirPath = tmpDir || process.env.TMPDIR || os.tmpdir();
+
+    return path.join(tmpDirPath, `${name}${suffix}`);
 }
